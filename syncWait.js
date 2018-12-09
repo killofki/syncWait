41ed513bfa589e77b6ca46653f6379d4e6d4b64c
killofki <killofki@yahoo.co.kr>
2018-12-10 05:59:02 +0900

    with setter & iterator

--- syncWait.js
+++ syncWait.js
@@ -15,15 +15,24 @@ Promise .all( [ Array( 10001 ), Array( 10 ) ] .map( aa =>
 	; 
 
 function * iMap100( a, F = v => v, { count = 100 } = {} ) { 
-	let ooa = []; 
+	let 
+		  ooa = [] 
+		, iput = { 
+			  [ Symbol .iterator ] : function *(){ 
+				let ii = this .ii; 
+				yield a[ ii ]; yield ii; yield a; 
+				} 
+			, foundProperty : function ( ii ) { return a .hasOwnProperty( 
+				this .ii = ii 
+				); } 
+			, ii : 0 
+			} 
+		; 
 	for ( let ai = 0; ai < a .length; ai += count ) { 
 		let oa = []; 
-		for ( let i = 0; i < count; i += 1 ) { 
-			let ii = ai + i; 
-			if ( a .hasOwnProperty( ii ) ) { 
-				oa .push( F( a[ ii ], ii, a ) ); 
-				} 
-			} 
+		for ( let i = 0; i < count; i += 1 ) { if ( iput .foundProperty( ai + i ) ) { 
+			oa .push( F( ... iput ) ); 
+			} } 
 		yield oa; 
 		ooa .push( oa ); 
 		} 
