096546d877c2e513655caeb269ee94f6ce75111e
killofki <killofki@yahoo.co.kr>
2019-12-14 00:20:42 +0900

    tabs & ..

--- syncWait.js
+++ syncWait.js
@@ -1,58 +1,67 @@
-"use module"; 
+"use module" 
 
-iGet( [ 1, 2, 3 ], { 
-	  checker : q => q > 1 ? delivery( 1000, q + 1 ) : q 
-	, res : v => console .log( v, '123' ) 
-	} ); 
+iGet( [ 1, 2, 3 ], new class { 
+	checker = q => q > 1 ? delivery( 1000, q + 1 ) : q 
+	res = v => console .log( v, '123' ) 
+	} ) 
 
-[ 1000001, 10 ] 
-.map( n => Array( n ) ) 
-.map( aa => iGet( iMap100( aa, v => v, { splitcount : 500000 } ), { 
-	  checker : async q => ( 
-		  console .log( await delivery( 0, q ), aa, 'checker' ) 
-		, q 
-		) 
-	, res : v => console .log( v, 'res' ) 
-	} ) ) 
-	; 
-iGet( ( function * () { yield 1; yield 2; yield 3; } )(), { 
-	  checker : v => ( delivery( 1000 ), v ) 
-	, res : v => console .log( v, 'async iterator' ) 
-	} ); 
+; [ 1000001, 10 ] 
+	.map( n => Array( n ) ) 
+	.map( aa => iGet( iMap100( aa, v => v, { splitcount : 500000 } ), { 
+		  checker : async q => { 
+			console .log( await delivery( 0, q ), aa, 'checker' ) 
+			return q 
+			} 
+		, res : v => console .log( v, 'res' ) 
+		} ) ) 
 
-var module = module || {}; 
-Object .assign( module .exports = module .exports || {}, { 
-	iMap100, iGet, switchtoPromise 
-	} ); 
+iGet( 
+	  ( function * () { 
+		yield 1 
+		yield 2 
+		yield 3 
+		} )() 
+	, new class { 
+		checker = v => ( delivery( 1000 ), v ) 
+		res = v => console .log( v, 'async iterator' ) 
+		} 
+	) 
+
+var module = module || {} 
+Object .assign( 
+	  module .exports = module .exports || {} 
+	, { iMap100, iGet, switchtoPromise } 
+	) 
 
 function * iMap100( a, F = v => v, { splitcount = 100 } = {} ) { 
-	   ( splitcount >= 1 ) 
+	;  ( splitcount >= 1 ) 
 	|| ( splitcount = 1 ) // guard about no result 
-		; 
-	let ooa = []; 
+	
+	let ooa = [] 
 	for ( let ai = 0; ai < a .length; ai += splitcount ) { 
-		let oa = []; 
-		for ( let i = 0; i < splitcount; i += 1 ) { if ( a .hasOwnProperty( ai + i ) ) { 
-			oa[ i ] = F( a[ ai + i ] ); 
-			} } 
-		pipe( 
-			  a .length - ai 
-			, oal => oa .length = oal > splitcount ? splitcount : oal 
-			); 
-		yield oa; 
-		ooa .push( oa ); 
+		let oa = [] 
+		for ( let i = 0; i < splitcount; i += 1 ) { 
+			if ( a .hasOwnProperty( ai + i ) ) { 
+				let v = a[ ai + i ] 
+				oa[ i ] = F( v ) 
+				} 
+			} 
+		let oal = a .length - ai 
+		oa .length = oal > splitcount ? splitcount : oal 
+		yield oa 
+		ooa .push( oa ) 
 		} 
-	return [] .concat( ... ooa ); // done with final 
+	return [] .concat( ... ooa ) // done with final 
 	} // -- iMap100() 
 
 function iGet( itv, { checker = v => v, res } = {} ) { 
 	// res = v => console .log( v ) 
-	   ( itv .next instanceof Function ) 
+	;  ( itv .next instanceof Function ) 
 	|| ( itv = itv[ Symbol .iterator ]() ) // error or catch iterator obj 
-		; 
-	let oa = [], itvn; 
-	for( 
-			  let value, done 
+	
+	let oa = [] 
+	let itvn 
+	for( let value, done 
 			; itvn = itv .next() 
 			, { value, done } = itvn 
 			, done && res ? res( [] .concat( ... oa ) ) 
@@ -60,8 +69,7 @@ function iGet( itv, { checker = v => v, res } = {} ) {
 					whileF 
 					}) // break with return  
 			, done === false // continue 
-			; 
-			) { 
+			; ) { 
 		let v = checker( value ); 
 		if ( v instanceof Promise ) { return switchtoPromise({ 
 			  preF : async q => ( 
@@ -120,4 +128,4 @@ function delivery( delay, v = delay ) { return new Promise( res =>
 	setTimeout( q => res( v ), delay ) 
 	); } // -- delivery() 
 
-function pipe( ... ar ) { return ar .reduce( ( o, F ) => F( o ) ); } // -- pipe() 
+function pipeReduce( ... ar ) { return ar .reduce( ( o, F ) => F( o ) ); } // -- pipeReduce() 
