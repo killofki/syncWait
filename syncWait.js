e470166097e6f356f4f70d984121e3a26b7533b5
김기웅 <killofki@yahoo.co.kr>
2022-12-14 00:49:11 +0900

    Squashed : merging tag20181212_23

--- syncWait.js
+++ syncWait.js
@@ -5,7 +5,7 @@ iteratorGet( [ 1, 2, 3 ], {
 
 [ 1000001, 10 ] 
 .map( n => Array( n ) ) 
-.map( aa => iteratorGet( iMap100( aa, v => v, { count : 500000 } ), { 
+.map( aa => iteratorGet( iMap100( aa, v => v, { splitcount : 500000 } ), { 
 	  checker : async q => ( 
 		  console .log( await delivery( 0, q ), aa, 'checker' ) 
 		, q 
@@ -14,25 +14,25 @@ iteratorGet( [ 1, 2, 3 ], {
 	} ) ) 
 	; 
 
-function * iMap100( a, F = v => v, { count = 100 } = {} ) { 
-	if ( count < 1 ) { 
-		count = 1; // guard about no result 
+function * iMap100( a, F = v => v, { splitcount = 100 } = {} ) { 
+	if ( splitcount < 1 ) { 
+		splitcount = 1; // guard about no result 
 		} 
 	let ooa = []; 
-	for ( let ai = 0; ai < a .length; ai += count ) { 
+	for ( let ai = 0; ai < a .length; ai += splitcount ) { 
 		let oa = []; 
-		for ( let i = 0; i < count; i += 1 ) { if ( a .hasOwnProperty( ai + i ) ) { 
+		for ( let i = 0; i < splitcount; i += 1 ) { if ( a .hasOwnProperty( ai + i ) ) { 
 			oa[ i ] = F( a[ ai + i ] ); 
 			} } 
 		pipe( 
 			  a .length - ai 
-			, m => oa .length = m > count ? count : m 
+			, m => oa .length = m > splitcount ? splitcount : m 
 			); 
 		yield oa; 
 		ooa .push( oa ); 
 		} 
 	return [] .concat( ... ooa ); // done with final 
-	} 
+	} // -- iMap100() 
 
 function iteratorGet( itv, { checker = v => v, res } = {} ) { 
 	if ( ! ( itv .next instanceof Function ) ) { 
@@ -42,28 +42,32 @@ function iteratorGet( itv, { checker = v => v, res } = {} ) {
 	for( 
 			  let value, done
 			; { value, done } = itv .next()
-			, done && res && res( [] .concat( ... oa ) ) 
-			, ! done
+			, done && res ? res( [] .concat( ... oa ) ) 
+				: true // continue 
 			; 
 			) { 
 		let v = checker( value ); 
-		if ( v instanceof Promise ) { return ( async ( 
-				  itnF = Pres => async ( { value, done } = itv .next() ) => ( 
-					  done ? ( res && res( [] .concat( ... oa ) ), Pres( oa ) ) 
-					: ( oa .push( await checker( value ) ), itn() ) 
-					) 
-				, itn 
-				) => (  
-			  oa .push( await v ) 
-			, new Promise( Pres => ( itn = itnF( Pres ), itn() ) ) 
-			) )(); } // push catched value & next.. 
+		if ( v instanceof Promise ) { 
+			return switchtoPromise( v ); 
+			} 
 		oa .push( v ); 
 		} 
 	return oa;  
-	} 
+	
+	async function switchtoPromise( v 
+			, itnF = Pres => async ( { value, done } = itv .next() ) => ( 
+				  done ? ( res && res( [] .concat( ... oa ) ), Pres( oa ) ) 
+				: ( oa .push( await checker( value ) ), itn() ) 
+				) 
+			, itn 
+			) { 
+		oa .push( await v ); 
+		return new Promise( Pres => ( itn = itnF( Pres ), itn() ) ); 
+		} // -- switchtoPromise() 
+	} // -- iteratorGet() 
 
 function delivery( delay, v = delay ) { return new Promise( res => 
 	setTimeout( q => res( v ), delay ) 
-	); } 
+	); } // -- delivery() 
 
-function pipe( ... ar ) { return ar .reduce( ( o, F ) => F( o ) ); } 
+function pipe( ... ar ) { return ar .reduce( ( o, F ) => F( o ) ); } // -- pipe() 
