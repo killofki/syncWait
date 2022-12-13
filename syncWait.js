82503fc18d7713fd1b457cadc3ec1977e075ea13
김기웅 <killofki@yahoo.co.kr>
2022-12-14 00:49:09 +0900

    Squashed : merging tag20181210_22

--- syncWait.js
+++ syncWait.js
@@ -1,66 +1,63 @@
-Promise .all([ iteratorGet( [ 1, 2, 3 ] 
-	, q => q > 1 ? new Promise( res => setTimeout( r => res( q + 1 ), 1000 ) ) 
-		: q 
-	) ]) 
-.then( pa => console .log( ... pa ) ) 
-	; 
-Promise .all( [ Array( 1000001 ), Array( 10 ) ] .map( aa => 
-	iteratorGet( 
-		  iMap100( [ ... aa ], v => v, { count : 500000 } ) 
-		, async q => ( console .log( await delivery( 0, q ), aa ), q ) 
+iteratorGet( [ 1, 2, 3 ], { 
+	  checker : q => q > 1 ? delivery( 1000, q + 1 ) : q 
+	, res : v => console .log( v, '123' ) 
+	} ); 
+
+[ 1000001, 10 ] 
+.map( n => Array( n ) ) 
+.map( aa => iteratorGet( iMap100( aa, v => v, { count : 500000 } ), { 
+	  checker : async q => ( 
+		  console .log( await delivery( 0, q ), aa, 'checker' ) 
+		, q 
 		) 
-	.then( a => a .flatMap( aa => aa ) ) 
-	) ) 
-.then( v => console .log( v ) ) 
+	, res : v => console .log( v, 'res' ) 
+	} ) ) 
 	; 
 
 function * iMap100( a, F = v => v, { count = 100 } = {} ) { 
-	let 
-		  ooa = [] 
-		, iput = { 
-			  [ Symbol .iterator ] : function *(){ 
-				let ii = this .ii; 
-				yield a[ ii ]; yield ii; yield a; 
-				} 
-			, foundProperty ( ii ) { return a .hasOwnProperty( 
-				this .ii = ii 
-				); } 
-			, set value( v ) { this .oa[ this .ii ] = v; } 
-			, set max( m ) { pipe( m, v => this .oa .length = v > count ? count : v ); } 
-			, ii : 0 
-			, oa : [] 
-			} 
-		; 
-	for ( let ai = 0; ai < a .length; ai += count ) { 
-		iput .oa = []; 
-		for ( let i = 0; i < count; i += 1 ) { if ( iput .foundProperty( ai + i ) ) { 
-			iput .value = F( ... iput ); 
-			} } 
-		iput .max = a .length - ai; 
-		yield iput .oa; 
-		ooa .push( iput .oa ); 
+	if ( count < 1 ) { 
+		count = 1; // guard about no result 
 		} 
-	return ooa .flatMap( v => v ); // done with final 
+	let ooa = []; 
+	for ( let ai = 0; ai < a .length; ai += count ) { 
+		let oa = []; 
+		for ( let i = 0; i < count; i += 1 ) { if ( a .hasOwnProperty( ai + i ) ) { 
+			oa[ i ] = F( a[ ai + i ] ); 
+			} } 
+		pipe( 
+			  a .length - ai 
+			, m => oa .length = m > count ? count : m 
+			); 
+		yield oa; 
+		ooa .push( oa ); 
+		} 
+	return [] .concat( ... ooa ); // done with final 
 	} 
 
-function iteratorGet( itv, F = v => v ) { 
+function iteratorGet( itv, { checker = v => v, res } = {} ) { 
 	if ( ! ( itv .next instanceof Function ) ) { 
 		itv = itv[ Symbol .iterator ](); // error or catch iterator obj 
 		} 
 	let oa = []; 
-	for( let value, done; { value, done } = itv .next(), ! done; ) { 
-		let v = F( value ); 
-		if ( v instanceof Promise ) { return ( async q => (  
+	for( 
+			  let value, done
+			; { value, done } = itv .next()
+			, done && res && res( [] .concat( ... oa ) ) 
+			, ! done
+			; 
+			) { 
+		let v = checker( value ); 
+		if ( v instanceof Promise ) { return ( async ( 
+				  itnF = Pres => async ( { value, done } = itv .next() ) => ( 
+					  done ? ( res && res( [] .concat( ... oa ) ), Pres( oa ) ) 
+					: ( oa .push( await checker( value ) ), itn() ) 
+					) 
+				, itn 
+				) => (  
 			  oa .push( await v ) 
-			, new Promise( res => { 
-				let itn = async ( { value, done } = itv .next() ) => ( 
-					  done ? res( oa ) 
-					: ( oa .push( await F( value ) ), itn() ) 
-					); 
-				itn(); 
-				} ) 
+			, new Promise( Pres => ( itn = itnF( Pres ), itn() ) ) 
 			) )(); } // push catched value & next.. 
-		oa .push( F( value ) ); 
+		oa .push( v ); 
 		} 
 	return oa;  
 	} 
